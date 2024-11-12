c256ab5ee5b4494fc5658acf0cb0527e8c8ab61b
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-11-12 12:58:07 +0600

    Add files via upload

--- pop.cpp
+++ pop.cpp
@@ -1,23 +1,18 @@
-#include <iostream>
+#include<bits/stdc++.h>
+#include<stack>
 using namespace std;
 
-int main() {
-    int MAXSTK = 100;  // Maximum size of the stack
-    int STACK[MAXSTK];  // Array to hold stack elements
-    int TOP = 0;  // Initialize TOP to 0 (indicating an empty stack)
-    int ITEM;  // Variable to hold the popped item
-
-    // Push an item onto the stack
-    STACK[++TOP] = 10;  // Increment TOP and then assign the value to STACK
-
-    // Pop operation with underflow check
-    if (TOP == 0) {
-        cout << "UNDERFLOW" << endl;
-    } else {
-        ITEM = STACK[TOP];  // Get the top item
-        TOP = TOP - 1;  // Decrease TOP by 1
-        cout << "Popped item: " << ITEM << endl;
+void stack_pop(){
+    stack<int>st;
+    st.push(10);
+    st.push(20);
+    while (!st.empty())
+    {
+        cout<<st.top()<<"\n";
+        st.pop();
     }
-
-    return 0;
+    
+}
+int main(){
+    stack_pop();
 }
